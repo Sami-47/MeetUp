fd08039d3d08148a657213170d8cbcbfeaed321f
sam47-asfaw <sami.amare47@gmail.com>
2022-02-25 09:19:09 +0300

    Added ef core migrations and updated database

---
+++ MeetUp/MeetUp/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MeetUp.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string UserName { get; set;}
+        public string Password { get; set;}
+        public byte[] ProfilePicture { get; set;}
+        public List<string> Followers { get; set;}
+
+        public List<Posts> Posts { get; set;}
+    }
+}
---
+++ MeetUp/MeetUp/Models/Posts.cs
@@ -0,0 +1,9 @@
+﻿namespace MeetUp.Models
+{
+    public class Posts
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string UserName { get; set; }
+    }
+}
